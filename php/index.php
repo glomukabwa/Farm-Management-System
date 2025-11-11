e3018845dccee0bf045e269da88d6f1f8d8fca42
mukabwag <mukabwaglo@gmail.com>
2025-11-11 12:48:14 +0300

    Overview Intro

---
+++ php/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overview Page</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+    <section class="sidebar">
+        <div>
+            <p>MF</p>
+        </div>
+
+        <div>
+            <div>
+                <a href="http://localhost/Farm%20Website/php/index.php">OVERVIEW</a>
+                <a href="#">ENTER FEED</a>
+                <a href="#">ENTER SALE</a>
+                <a href="#">PRODUCTS</a>
+                <a href="#">Milk</a>
+                <a href="#">Bulls</a>
+                <a href="#">Chicken</a>
+                <a href="#">Eggs</a>
+                <a href="#">Pigs</a>
+                <a href="#">Kales</a>
+                <a href="#">Maize</a>
+            </div>
+
+            <div>
+                <a href="#">PROFILE</a>
+                <a href="#">SETTINGS</a>
+                <a href="#">LOG OUT</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="maincontent">
+        <div>
+            <h1>Hello User</h1>
+
+            <div>
+                <h2>Today's Feeding</h2>
+                <table>
+                    <tr>
+                        <th></th>
+                        <th>Morning Meal</th>
+                        <th>Evening Meal</th>
+                        <th>Water Refill</th>
+                    </tr>
+                    <tr>
+                        <td>Cows</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                    </tr>
+                    <tr>
+                        <td>Calves</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                    </tr>
+                    <tr>
+                        <td>Bulls</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                    </tr>
+                    <tr>
+                        <td>Pigs</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                    </tr>
+                    <tr>
+                        <td>Piglets</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                    </tr>
+                    <tr>
+                        <td>Chicken</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                        <td>✕</td>
+                    </tr>
+                </table>
+                <a href="#">EDIT</a>
+            </div>
+
+            <div>
+                <h2>Weekly Sales Report</h2>
+            </div>
+        </div>
+
+        <div>
+            <h1>Alerts</h1>
+            <p>No alerts found</p>
+            <a href="#">See More</a>
+        </div>
+    </section>
+</body>
+</html>
