9778db1d971fcc893ea6e19d4adb292988430ad1
mukabwag <mukabwaglo@gmail.com>
2026-01-13 08:16:06 +0300

    db edit

--- DATABASE.sql
+++ DATABASE.sql
@@ -25,6 +25,7 @@ CREATE TABLE users (-- record of all existing animals
     id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(20) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     role ENUM('admin','manager','staff') NOT NULL,
     status ENUM('pending','approved','suspended') DEFAULT 'pending',
