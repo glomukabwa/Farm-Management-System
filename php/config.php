60b281bce93fbc99e712d5571ee08d9cae1326a2
mukabwag <mukabwaglo@gmail.com>
2026-01-14 05:41:25 +0300

    Database Connection

---
+++ php/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost:3307";
+$user = "root";
+$password = "b@bad1ana";
+$dbname = "farm_management";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
