d2917afda3b3f42f305f62bd849e5f5ac107e0ad
mukabwag <mukabwaglo@gmail.com>
2026-06-25 10:28:56 +0300

    Index.php - I've changed the Weekly Sales Chart to a bar chart

--- js/index.js
+++ js/index.js
@@ -14,80 +14,16 @@ const salesData = JSON.parse(salesContent.dataset.values);
 */
 
 new Chart(salesChart, {
-    type: 'line',
+    type: 'bar',
     data: {
         labels: salesLabels,
         datasets: [
             {
-                label: 'Weekly Sales',
+                label: 'Ksh',
                 data: salesData,
-                borderWidth: 2,
-                tension:0.2,
-                borderColor: 'black',
-                backgroundColor: 'black', //Fills the nodes on the chart and the legend with this color
-                pointRadius: 0 //Removes the nodes
+                borderWidth: 2
             }
-        ],
-    },
-    options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                // Legend-level options
-                align: 'end', //Makes it move to the right
-
-                // Options specifically for the **text and symbol** in the legend
-                labels: {
-                    usePointStyle: true,   // makes the legend symbol a shape (circle, triangle, etc.)
-                    pointStyle: 'circle',  // options: 'circle', 'triangle', 'rect', 'rectRounded', 'rectRot', 'cross', 'star', 'line', 'dash'
-                    color: 'black',     // legend text color
-                    //pointStyleWidth: 15,   //Resizes the circle width
-                    pointStyleWidth: 12, // circle width
-                    boxWidth: 12, //This is the space allocated to the circle so this is the only way to controll the height
-                    font: { size: 10 },
-                    //padding: 10            // space around the legend items
-                }
-            }
-        },
-        scales: {
-            x: {
-                offset: true,// Makes the labelse not begind and end at the edge. Chat says it gives the axis padding to the left and right
-                border: {
-                    display: true,
-                    color: 'black',
-                    width: 2
-                },
-                grid: {
-                    display: true, //false removes the vertical grid lines
-                    tickLength: 10, //Ticks are the lines after the axes that point to the label
-                    tickWidth: 2,
-                    tickColor: 'black' //This affects the color of the ticks
-                },
-                ticks: {
-                    color: 'black' // This affects the color of the label themselves
-                }
-            },
-            y: {
-                offset: true,
-                border: {
-                    display: true,
-                    color: 'black',
-                    width: 2
-                },
-                grid: {
-                    display: true, //false removes the horizontal grid lines
-                    tickColor: 'black',
-                    tickWidth: 2,
-                    tickLength: 10   
-                },
-                ticks: {
-                    color: 'black'
-                },
-                beginAtZero:true,
-                suggestedMax: 1000000, // There's max and suggestedMax. max is fixed while suggestedMax is better cz it allows chart to exceed 1M if needed
-                //min: 0 // This is supposed to ensure the line doesn't bend even slightly below zero but for some reason, it isn't working. The tension is what is making it so flexible so I'm gonna reduce the tension instead
-            }
-        }
+        ]
     }
 });
 
